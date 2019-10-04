b6e8bd1644e13a58fad3712e38ddf4b4ed935685
Denys Safonov <safonov@etcetera.kiev.ua>
2019-10-04 11:05:53 +0200

    to_notion

--- app.py
+++ app.py
@@ -95,11 +95,14 @@ def createMessage(token, parent_page_url, message):
     date = NotionDate(datetime.now())
     page = client.get_block(parent_page_url)
     a=page.children.add_new(DividerBlock)
-    b=page.children.add_new(TextBlock, title=date.toNotion + " " + message)
+    b=page.children.add_new(TextBlock, title=date.to_notion + " " + message)
     c=page.children.add_new(DividerBlock)
     a.move_to(page, "first-child")
     b.move_to(a, "after")
     c.move_to(b, "after")
+     
+     
+     
      
 @app.route('/message', methods=['GET'])
 def message():
