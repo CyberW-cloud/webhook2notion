fc470513473664fea9dffbf476b52e5c153c8233
Denys Safonov <safonov@etcetera.kiev.ua>
2019-10-04 21:46:04 +0200

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from notion.client import NotionClient
 from notion.block import DividerBlock, TextBlock, HeaderBlock
 from notion.collection import NotionDate
-from datetime import datetime 
+from datetime import datetime, date
 from flask import Flask
 from flask import request
 import re
@@ -12,7 +12,81 @@ import re
 app = Flask(__name__)
 
 
+class NotionDate(object):
 
+    start = None
+    end = None
+    timezone = None
+
+    def __init__(self, start, end=None, timezone=None):
+        self.start = start
+        self.end = end
+        self.timezone = timezone
+
+    @classmethod
+    def from_notion(cls, obj):
+        if isinstance(obj, dict):
+            data = obj
+        elif isinstance(obj, list):
+            data = obj[0][1][0][1]
+        else:
+            return None
+        start = cls._parse_datetime(data.get("start_date"), data.get("start_time"))
+        end = cls._parse_datetime(data.get("end_date"), data.get("end_time"))
+        timezone = data.get("timezone")
+        return cls(start, end=end, timezone=timezone)
+
+    @classmethod
+    def _parse_datetime(cls, date_str, time_str):
+        if not date_str:
+            return None
+        if time_str:
+            return datetime.strptime(date_str + " " + time_str, "%Y-%m-%d %H:%M")
+        else:
+            return datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    def _format_datetime(self, date_or_datetime):
+        if not date_or_datetime:
+            return None, None
+        if isinstance(date_or_datetime, datetime):
+            return (
+                date_or_datetime.strftime("%Y-%m-%d"),
+                date_or_datetime.strftime("%H:%M"),
+            )
+        else:
+            return date_or_datetime.strftime("%Y-%m-%d"), None
+
+    def type(self):
+        name = "date"
+        if isinstance(self.start, datetime):
+            name += "time"
+        if self.end:
+            name += "range"
+        return name
+
+    def to_notion(self):
+
+        if self.end:
+            self.start, self.end = sorted([self.start, self.end])
+
+        start_date, start_time = self._format_datetime(self.start)
+        end_date, end_time = self._format_datetime(self.end)
+
+        if not start_date:
+            return []
+
+        data = {"type": self.type(), "start_date": start_date, "date_format": "relative" }
+
+        if end_date:
+            data["end_date"] = end_date
+
+        if "time" in data["type"]:
+            data["time_zone"] = "Etc/UTC"
+            data["start_time"] = start_time or "00:00"
+            if end_date:
+                data["end_time"] = end_time or "00:00"
+
+        return [["‣", [["d", data]]]]
 
 def trackWeather(token, URL, weather):
     # notion
@@ -113,8 +187,7 @@ def createMessageDATE(token, parent_page_url, message):
     date = datetime.now()
     page = client.get_block(parent_page_url)
     a = page.children.add_new(TextBlock, title=" ")
-#   c = page.children.add_new(TextBlock, title = "{data} {msg}".format(data = NotionDate(datetime.today(), None, 'Etc/UTC'), msg = message))
-    b = page.children.add_new(TextBlock, title = "{start}{data}{end} {msg}".format(start = '[["‣", [["d", {"type": "date", "start_date": "', data = datetime.now().strftime("%Y-%m-%d"), end = '", "date_format": "relative"}]]]]' , msg = message))
+    b = page.children.add_new(TextBlock, title = "{data} {msg}".format(data = NotionDate(datetime.date.today()), msg = message))
     a.move_to(page, "first-child")
     b.move_to(a, "after")
 
