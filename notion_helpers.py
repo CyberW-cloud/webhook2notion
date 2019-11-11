cc1301af9d7e37708f8e7623e2c0d64833c340a2
Denys Safonov <safonov@etcetera.kiev.ua>
2019-11-11 20:19:31 +0100

    .

--- notion_helpers.py
+++ notion_helpers.py
@@ -90,7 +90,7 @@ def create_new_task(page, header, date, text, timezone, tasks):
         for task in tasks:
             td = parent.children.add_new(TodoBlock, title=task)
             td.checked = False
-            if  text:
+            if  text: 
                td.move_to(tx, "first-child") 
             td.move_to(parent, "after")                    
             parent = td
