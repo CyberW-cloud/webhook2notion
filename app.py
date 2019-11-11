bfc345f7fb514d246ee16f15523967e591cc6991
Denys Safonov <safonov@etcetera.kiev.ua>
2019-11-11 18:10:03 +0100

    .

--- app.py
+++ app.py
@@ -460,8 +460,9 @@ def todo_one():
     todo = "{}".format(request.args.get("todo")).split("||")
     text = request.args.get("text")
     date = request.args.get("date", None)
-    if todo_url.find("https://www.notion.so") == -1: 
-        todo_url = get_toto_url_by_name(token_v2, member)
+    if member.find("https://www.notion.so") != -1: 
+        todo_url = member
+    else todo_url = get_toto_url_by_name(token_v2, member)
     if todo_url is not None:
         create_todo(token_v2, date, todo_url, todo, text)
         return f'added to {member} {text if text else ""} {todo}  to Notion'
