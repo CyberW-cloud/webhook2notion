d41444f1901d5b486b4646a3053986e72e7347f7
anton <anton.chasnyk@gmail.com>
2019-11-18 14:25:54 +0200

    comments for notion_helpers

--- notion_helpers.py
+++ notion_helpers.py
@@ -6,13 +6,13 @@ import datetime
 
 
 def get_date_from_title(title):
-    if isinstance(title, list):
+    if isinstance(title, list):  # instance must be list
         for el in title:
-            if len(el) < 2:
+            if len(el) < 2:      # element does not contain special markdown
                 continue
             else:
                 try:
-                    if el[1][0][0] == 'd':
+                    if el[1][0][0] == 'd':  # if exist date return it
                         return el
                 except Exception:
                     raise Exception('Unexpected format')
@@ -35,12 +35,13 @@ def get_user_id_from_title(title):
         return None
 
 
+# looking for headers with target date or return last header
 def get_previous_or_target_headers(page, target_date):
-    prev_date = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d').date()
+    prev_date = datetime.datetime.strptime('1900-01-01', '%Y-%m-%d').date() # start date for comparison
     store = page.children[0]
     for child in page.children:
         block_type = child.get('type')
-        if 'header' in block_type:
+        if 'header' in block_type:              # check only headers block
             prop = child.get('properties')
             if prop is None:
                 continue
@@ -48,25 +49,25 @@ def get_previous_or_target_headers(page, target_date):
             date = get_date_from_title(title)
             if date:
                 d = NotionDate.from_notion([date])
-                if isinstance(d.start, datetime.datetime):
+                if isinstance(d.start, datetime.datetime):  # check only date, ignore time
                     date = d.start.date()
                 else:
                     date = d.start
-                if date == target_date:
+                if date == target_date:             # if date is same return exact date
                     return 'exact', child
                 else:
-                    if prev_date < date < target_date:
+                    if prev_date < date < target_date:  # update last item and continue searching
                         store = child
                         prev_date = date
     return 'prev', store
 
 
+# move task (header, text and list before block)
 def move_task_before(task, block):
     task['header'].move_to(block, "before")
     try:
-        if task['text']:
+        if task['text']:     # all child will be move automatically
             task['text'].move_to(task['header'], "after")
-            #task['to-do'][0].move_to(task['text'], "after")
         else:
             task['to-do'][0].move_to(task['header'], "after")
             for num, td in enumerate(task['to-do'][1:]):
@@ -77,17 +78,17 @@ def move_task_before(task, block):
 
 def create_new_task(page, header, date, text, timezone, tasks):
     type, parent = get_previous_or_target_headers(page, date)
-    if type == 'exact':
-        if header:
+    if type == 'exact':     # if header exist
+        if header:  # add title to header
             prop = parent.get('properties')
             prop['title'].append([' '])
             prop['title'].append([header])
             parent.set('properties', prop)
-        if text:
+        if text:    # add text to task
             tx = page.children.add_new(TextBlock, title=text)
             tx.move_to(parent, "after")
             parent = tx
-            for task in tasks:
+            for task in tasks:  # if text exist add todos as children of text block
                 td = parent.children.add_new(TodoBlock, title=task)
                 td.checked = False
         else:
@@ -95,9 +96,8 @@ def create_new_task(page, header, date, text, timezone, tasks):
                 td = parent.children.add_new(TodoBlock, title=task)
                 td.checked = False
                 td.move_to(parent, "after")
-            #parent = td
 
-    else:
+    else:   # if header not exist create new header and add a task
         title = NotionDate(date, timezone=timezone).to_notion()
         if header:
             title.append([' '])
