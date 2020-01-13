72320d7de32ef8dbf24a49af1ee0dc6481295f07
Denys Safonov <safonov@etcetera.kiev.ua>
2020-01-13 11:25:26 +0100

    time.sleep(3)

--- notion_helpers.py
+++ notion_helpers.py
@@ -103,6 +103,7 @@ def create_new_task(page, header, date, text, timezone, tasks):
             title.append([' '])
             title.append([header])
         new_child = page.children.add_new(HeaderBlock, title=" . ")
+        time.sleep(3)
         prop = new_child.get('properties')
         prop['title'] = title
         new_child.set('properties', prop)
