563ee58c7bf648cb67cc2b3917017506f2487aa4
Denys Safonov <safonov@etcetera.kiev.ua>
2020-06-18 14:39:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -477,13 +477,13 @@ def weekly_todo_fl(token, staff, calendar):
 def friday_todo_fl(token, staff, calendar):
     print("Fri Fl's start")
     for fl in staff:
-        if fl['name'] != 'Denys Safonov':
-            continue
+#        if fl['name'] != 'Denys Safonov':
+#            continue
             print(f"FL {fl['name']} start")
             # Friday
-            todo = list()
-            todo.append("Коментом напиши какие day-off ты планируешь на следующую неделю и тегни своего PA, иначе напиши - Не планирую")
-            create_todo(token, calendar["fri"], fl["todo_url"], todo, text="")
+#            todo = list()
+#            todo.append("Коментом напиши какие day-off ты планируешь на следующую неделю и тегни своего PA, иначе напиши - Не планирую")
+#            create_todo(token, calendar["fri"], fl["todo_url"], todo, text="")
     print("Fri FL done")    
 
 def weekly_todo_bidder(token, staff, calendar):
@@ -638,7 +638,7 @@ def friday_todo():
             friday_todo_fl(token_v2, staff[role], dates)    
         else:
             return f"Can't find Function for role {role}"
-    print(f"fiday todo for {roles} done")
+    print(f"Fiday todo for {roles} done")
     return "Done!"
 
 
