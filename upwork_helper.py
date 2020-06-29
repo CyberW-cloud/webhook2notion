d242b51338077d2eda1a8e23fc266e14e78f4487
Denys Safonov <safonov@etcetera.kiev.ua>
2020-06-29 13:36:33 +0300

    Revert "Update upwork_helper.py"
    This reverts commit c6a0f503ab9585efbc8c61599aa30a8fb2588cf3.

--- upwork_helper.py
+++ upwork_helper.py
@@ -2,14 +2,14 @@ import upwork
 from upwork.routers import auth
 
 def get_desktop_client():
-    config = upwork.Config({
+     config = upwork.Config({
           'consumer_key': os.environ.get("ConsumerKey"),            
           'consumer_secret': os.environ.get("ConsumerSecret"),            
           'access_token': os.environ.get("AccessToken"),            
           'access_token_secret': os.environ.get("AccessSecret")})
      
-    client = upwork.Client(config)
-    try:        
+     client = upwork.Client(config)
+     try:        
         config.access_token        
         config.access_token_secret
     except AttributeError:
