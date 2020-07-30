f2f06da97d3878d52fc8565610cc3b2f2bca3c23
nocommas555 <nocommas555@gmail.com>
2020-07-30 11:14:48 +0300

    moved to app.py

--- app.py
+++ app.py
@@ -14,6 +14,47 @@ timezone = "Europe/Kiev"
 
 app = Flask(__name__)
 
+@app.route("/todo_test", methods=["GET"])
+def update_todo()
+    app = Flask(__name__)
+
+    site = "https://www.notion.so/0aead28cb9f34ec2b41a9af19b96817a?v=1266d9ce8cbd4c968d29b4b877bed345"
+    token_v2 = os.environ.get("TOKEN")
+
+    client.get_collection_view(site)
+
+    n = datetime.datetime.now()
+    n = n.replace(hour=12, minute=0, second=0, microsecond=0) - datetime.timedelta(days=days_before)
+
+    # get projects InProgress with date less when now()-days before
+    filter_params = {
+        "filters": [
+            {
+                "filter": {"value": {"type": "exact", "value": "TODO"}, "operator": "enum_is"},
+                "property": "Status",
+            },
+            {
+                "property": "Updated",
+                "filter": {
+                    "operator": "date_is_on_or_before",
+                    "value": {
+                        "type": "exact",
+                        "value": {
+                            "type": "date",
+                            "start_date": str(n.date())
+                            # "start_date": '2020-03-19'
+                        },
+                    },
+                },
+            },
+        ],
+        "operator": "and",
+    }
+    cv = cv.build_query(filter=filter_params)
+    result = cv.execute()
+
+    result = nview_to_pandas(result)
+    print(result)
 
 def parse_staff(todo, table, obj, client_days_before):
     test_date = datetime.datetime.now()
