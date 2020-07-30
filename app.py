667dae51f9fc335d36c4c2e4aad68930832e0196
nocommas555 <nocommas555@gmail.com>
2020-07-30 15:20:30 +0300

    testing everything

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def todo_test():
 
 
 
-    return(str(todo.set_date))
+    return(str(todo.set_date.start))
 
 def parse_staff(todo, table, obj, client_days_before):
     test_date = datetime.datetime.now()
