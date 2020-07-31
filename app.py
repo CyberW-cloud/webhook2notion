80258de7a717915043151a2e9c2401eefce23c2d
nocommas555 <nocommas555@gmail.com>
2020-07-31 17:25:04 +0300

    testing how to use vars in notion

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def todo_test():
                 record.set_property("Due Date", i["due"])
                 record.set_property("Set date", i["set"])
     
-    return(s)
+    return(str(s))
 
 def parse_staff(todo, table, obj, client_days_before):
     test_date = datetime.datetime.now()
