667ec5489dcf77e4edd504ee480c1c5e2178375a
nocommas555 <nocommas555@gmail.com>
2020-08-09 21:24:55 +0300

    adding testing

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ def test_scripts():
 	
 	test_page_url = create_page(day_page.get_browseable_url(), "/kickstaff").get_browseable_url()
 
-	kick_staff()
+	#kick_staff()
 	
 	test_page_url = create_page(day_page.get_browseable_url(), "/weekly_todo").get_browseable_url()
 
-	weekly_todo()
+	#weekly_todo()
 
 	test_page_url = create_page(day_page.get_browseable_url(), "/friday_todo").get_browseable_url()
 
