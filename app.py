1b6e6d846bf1435ffc500fe04ea7ce85dccfc5ba
nocommas555 <nocommas555@gmail.com>
2020-08-11 20:40:44 +0300

    added retrying on failed write

--- app.py
+++ app.py
@@ -72,7 +72,8 @@ def test_scripts():
 	
 
 	day_page.children.add_new(CollectionViewPageBlock, title="test_table")
-	schema = client.get_block("https://www.notion.so/7113e573923e4c578d788cd94a7bddfa?v=375e91212fc4482c815f0b4419cbf5e3").collection.get_schema_properties()
+	schema = client.get_block("https://www.notion.so/7113e573923e4c578d788cd94a7bddfa?v=375e91212fc4482c815f0b4419cbf5e3").collection.get("schema")
+	print(schema)
 
 	collection = client.get_collection(client.create_record("TEST", parent=day_page.children[-1], schema=str(schema)))
 
