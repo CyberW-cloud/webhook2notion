d9b97d91459dbc7a366ff56714b9a4ee65d1a10b
nocommas555 <nocommas555@gmail.com>
2020-08-11 21:28:08 +0300

    testing how to test tables

--- app.py
+++ app.py
@@ -71,11 +71,10 @@ def test_scripts():
 	# 	return "Test FAILED!: " + str(e) + "\n" + str(''.join(traceback.format_exception(None, e, e.__traceback__)))
 	
 
-	day_page.children.add_new(CollectionViewPageBlock)
+	day_page.children.add_new(CollectionViewPageBlock, title = "test_table")
 	page = day_page.children[-1]
-	page.title = "test_table"
+
 	schema = client.get_block("https://www.notion.so/7113e573923e4c578d788cd94a7bddfa?v=375e91212fc4482c815f0b4419cbf5e3").collection.get("schema")
-	print(schema)
 
 	collection = client.get_collection(client.create_record("collection", parent=page, schema=schema))
 	page.collection = collection
