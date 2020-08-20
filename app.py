165e1c4b6c1e24e6a32ad110bfad73f7cfe7ec6a
Tech Team <tech@etcetera.kiev.ua>
2020-08-20 20:58:33 +0300

    writing getting current user id list

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def upwork_test():
 	company = companyAPI(client)
 
 	company_ref = [x["reference"] for x in company.get_list()["companies"]]
-	return company_ref
+	return str(company_ref)
 
 @app.route('/add_global_block', methods=["GET"])
 def add_global_block():
