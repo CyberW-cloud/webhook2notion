d9643e1b08aa5cc02be1fa5e7736ac8afb904315
Tech Team <tech@etcetera.kiev.ua>
2020-08-20 19:27:16 +0300

    testing get_room out

--- app.py
+++ app.py
@@ -32,12 +32,12 @@ test_page_url = "https://www.notion.so/Test-6745f90a3268473790a8070ec8434d4c"
 
 def parse_tokens(tokens):
 	tokens = str(tokens)
-	tokens = re.search("{(.*)}", tokens)
+	tokens = re.search("{(.*)}", tokens).groups()
 
 	ret = []
 	for i in range(len(tokens)):
 		try:
-			strings = re.search('"(.*)"', tokens[i]).group(1)
+			strings = re.search('"(.*)"', tokens[i]).groups()
 			ret.append({"id": strings[0], strings[1]:strings[2], strings[3]:strings[4]})
 		except Exception:
 			continue
