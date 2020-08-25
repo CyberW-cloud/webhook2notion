88014f52c4eb2f4aed86fda4c31efda6aeccf839
nocommas555 <nocommas555@gmail.com>
2020-08-25 18:37:02 +0300

    testing the script

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def upwork_test():
 		# we have to use range() to go in reverse
 		stories = room["messages"]["stories_list"]["stories"]
 		for i in range(len(stories)-1, 0 , -1):
-			time = datetime.datetime.fromtimestamp(stories[i]["updated"][:-3]).strftime('%Y-%m-%d %H:%M:%S')
+			time = datetime.datetime.fromtimestamp(stories[i]["updated"]/1000).strftime('%Y-%m-%d %H:%M:%S')
 			text = "["+time+"]\n"
 
 			name = "Client"
