817e85cd0394737dff77f4bceb5128e80501e2b0
nocommas555 <nocommas555@gmail.com>
2020-08-28 16:25:20 +0300

    changing the formatting

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def upwork_test():
 	
 	contracts = notion_client.get_collection_view("https://www.notion.so/5a95fb63129242a5b5b48f18e16ef19a?v=81afe49071ef41bba4c85922ff134407")
 	proposals = notion_client.get_collection_view("https://www.notion.so/99055a1ffb094e0a8e79d1576b7e68c2?v=bc7d781fa5c8472699f2d0c1764aa553")
-	message_review = notion_client.get_collection_view("https://www.notion.so/d134162fbfb14449a7ae426487f56127?v=159b522f95fc460f9171dfdca6d1f6d8")
+	message_review = notion_client.get_block("https://www.notion.so/d134162fbfb14449a7ae426487f56127?v=159b522f95fc460f9171dfdca6d1f6d8")
 
 	tokens = os.environ.get('TOKENS')
 
