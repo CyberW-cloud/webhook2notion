b3306b5c0bd70cf77867ec864c9a9994f8c6afa1
nocommas555 <nocommas555@gmail.com>
2020-08-29 14:51:37 +0300

    fix the first message skipped bug

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def upwork_test():
 				proposals_found = []
 
 			try:
-				messages = messages_api.get_room_messages(user_id, room["roomId"], {"limit":5})
+				messages = messages_api.get_room_messages(user_id, room["roomId"], {"limit":15})
 			except Exception as e:
 				messages = []
 			
