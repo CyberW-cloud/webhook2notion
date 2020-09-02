c2a1ad8d783d1a1a6fdaad6b53f58a788f726c80
nocommas555 <nocommas555@gmail.com>
2020-09-02 18:30:05 +0300

    writing contracts

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def update_db_contracts():
 
 	
 	""" SINCE WHEN IS 3 " NOT A COMMENT?!?!?!?!?!?!? """
-	start_from_contracts = cur.execute("""Select MAX(Date) from contracts""")
-	start_from_proposals = cur.execute("""Select MAX(Date) from proposals""")
+	start_from_contracts = cur.execute("""Select MAX(Date) from contracts""").result
+	start_from_proposals = cur.execute("""Select MAX(Date) from proposals""").result
 
 	print(start_from_contracts)
 	filter_params = {
