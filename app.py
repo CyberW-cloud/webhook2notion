d5742e2bf59f46bf1a42dab9ea87bda77901f19c
nocommas555 <nocommas555@gmail.com>
2020-09-04 17:33:58 +0300

    writing contracts

--- app.py
+++ app.py
@@ -97,12 +97,8 @@ def update_db_contracts():
 
 
 	#remove duplicates (based on contract_id) 
-	cur.execute("""DELETE FROM
-    				contracts a
-        				USING contracts b
-					WHERE
-    					a.id < b.id
-    					AND a.contract_id = b.contract_id;""")
+	cur.execute("""DELETE FROM contracts a USING contracts b WHERE a.id < b.id AND a.contract_id = b.contract_id;""")
+	conn.commit()
 
 #accepted users should be an array of id's or "all" for accepting all users
 def parse_tokens(tokens, accepted_users = "all"):
