41e44828dc34fe834c3d42b360a407378c467db4
nocommas555 <nocommas555@gmail.com>
2020-09-04 18:39:59 +0300

    writing proposals

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def update_db_contracts():
 	
 	for row in result:
 
-		proposal_id = str(row.proposal_id)
+		proposal_id = re.findall("[0-9]{9,}",str(row.proposal_id))
 		if proposal_id == '':
 			proposal_id == "-999"
 
