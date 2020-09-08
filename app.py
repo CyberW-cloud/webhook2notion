ef4271b659a1b9c62f049ce13f87a50b579788cf
nocommas555 <nocommas555@gmail.com>
2020-09-08 17:34:51 +0300

    looking why kickstaff failed

--- app.py
+++ app.py
@@ -909,6 +909,8 @@ def get_projects(token, days_before):
 			project["person"] = row["pm"][0]
 		else:
 			for contract in row["contracts"]:
+				if row["name"] == "Brainy Apps":
+					print(contract)
 				if contract.coordinator[0]:
 					if contract.coordinator[0].name != "selfCC" and contract.status == "In Progress":
 						project["person"] = contract.coordinator[0]
