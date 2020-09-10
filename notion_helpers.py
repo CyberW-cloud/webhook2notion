d3f782937fed246c54b5b7676826df648c59e7d9
nocommas555 <nocommas555@gmail.com>
2020-09-10 14:46:37 +0300

    moving global block and activityLog code to helpers

--- notion_helpers.py
+++ notion_helpers.py
@@ -36,7 +36,7 @@ def get_activity_log(client, target_page):
 		"navigableBlockId":target_page.id,
 		"limit":20
 	}
-	client.post("getActivityLog", data).json()["recordMap"]["block"].values()
+	client.post("getActivityLog", data).json()["recordMap"]
 
 def get_date_from_title(title):
 	if isinstance(title, list):  # instance must be list
