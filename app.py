12a62a8ec1af38906a82c244c77cb128f18a22ac
nocommas555 <nocommas555@gmail.com>
2020-10-01 20:34:36 +0300

    adding the not updated text at the end

--- app.py
+++ app.py
@@ -525,15 +525,15 @@ def head_summary():
 
 		if len(todo[manager]["proposals"])>0:
 			if "Interviews" in rows.keys():
-				rows["Contracts"].children.add_new(TextBlock, title = "**Not Updated in "+str(proposal_days)+" days:**")
-				parent_block = rows["Proposals"].children.add_new(TextBlock, title = "["+manager+": ]("+todo[manager]["todo_url"]+")")
+				rows["Interviews"].children.add_new(TextBlock, title = "**Not Updated in "+str(proposal_days)+" days:**")
+				parent_block = rows["Interviews"].children.add_new(TextBlock, title = "["+manager+": ]("+todo[manager]["todo_url"]+")")
 				for i in todo[manager]["proposals"]:
 					parent_block.children.add_new(TextBlock, title = "["+i[0]+"]("+i[1]+")")
 				print(todo[manager]["proposals"])	
 			
 		if len(todo[manager]["projects"])>0:
 			if "Projects" in rows.keys():
-				rows["Contracts"].children.add_new(TextBlock, title = "**Not Updated in "+str(client_days_before)+" days:**")
+				rows["Projects"].children.add_new(TextBlock, title = "**Not Updated in "+str(client_days_before)+" days:**")
 				parent_block = rows["Projects"].children.add_new(TextBlock, title = "["+manager+": ]("+todo[manager]["todo_url"]+")")
 				for i in todo[manager]["projects"]:
 					parent_block.children.add_new(TextBlock, title = "["+i[0]+"]("+i[1]+")")
