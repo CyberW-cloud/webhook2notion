79dce1c51b29c8e089f0ec7e9107f4e1fb96a925
TECH TEAM <nocommas555@gmail.com>
2020-10-01 21:08:31 +0300

    add the kickstaff feature to head summary

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def head_summary():
     client = NotionClient(token)
 
 
-    target_table = client.get_block(request.args.get("table", "https://www.notion.so/96a6a203a3a446e4a8e0673682a2304b?v=81f1740cd5df439b9bf2ba3a25313dbb"))
+    target_table = client.get_block(request.args.get("table", "https://www.notion.so/d134162fbfb14449a7ae426487f56127?v=159b522f95fc460f9171dfdca6d1f6d8"))
     proposals = client.get_collection_view("https://www.notion.so/99055a1ffb094e0a8e79d1576b7e68c2?v=bc7d781fa5c8472699f2d0c1764aa553")
     contracts = client.get_collection_view("https://www.notion.so/5a95fb63129242a5b5b48f18e16ef19a?v=48599e7a184a4f32be2469e696367949")
     projects = client.get_collection_view("https://www.notion.so/addccbcaf545405292db498941c9538a?v=e86f54933acc461ca413afa6a2958cdc")
