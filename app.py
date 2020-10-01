0673548fb819cd07118a6a6d35b9a4358253e98c
nocommas555 <nocommas555@gmail.com>
2020-10-01 19:49:08 +0300

    adding the not updated text at the end

--- app.py
+++ app.py
@@ -523,7 +523,7 @@ def head_summary():
 					parent_block.children.add_new(TextBlock, title = "["+i[0]+"]("+i[1]+")")
 				print(todo[manager]["Contracts"])			
 
-		if len(todo[manager]["interviews"])>0:
+		if len(todo[manager]["proposals"])>0:
 			if "Interviews" in rows.keys():
 				rows["Contracts"].children.add_new(TextBlock, title = "**Not Updated in "+proposal_days+"days:**")
 				parent_block = rows["Proposals"].children.add_new(TextBlock, title = "["+manager+": ]("+todo[manager]["todo_url"]+")")
