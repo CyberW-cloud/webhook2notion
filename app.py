9e88c57827afc5975c91e7c647b9dcf0e5a1aa74
nocommas555 <nocommas555@gmail.com>
2020-10-09 20:26:57 +0300

    debugging weekly todo pa

--- app.py
+++ app.py
@@ -2181,7 +2181,7 @@ def get_todo_list_by_role(token, roles):
 			
 			for i, f in team_df.iterrows():
 				if f["name"] == "Serhii Maslovskiy":
-					print(f["pa name"])
+					print(f["pa_name"])
 				if f["pa_name"] == person["name"]:
 					print(person["name"] + " " + f["name"])
 					d["pa_for"].append((f["name"], f["row"].get_browseable_url()))
