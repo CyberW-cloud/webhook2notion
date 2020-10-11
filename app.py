d8b6cfa1514b538df34e22391230ac18343c1053
nocommas555 <nocommas555@gmail.com>
2020-10-11 17:09:54 +0300

    increased delay 3x, restarting

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ def tmp():
 	filter_params = {
 		"filters": [
 			{
-				"filter": {"operator": "date_is_on_or_before", "value": {"type": "exact", "value": {"type": "date", "start_date": "2020-04-01"}}},
+				"filter": {"operator": "date_is_on_or_before", "value": {"type": "exact", "value": {"type": "date", "start_date": "2020-02-20"}}},
 				"property": "Added",
 			}
 		],
