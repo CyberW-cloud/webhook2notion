bc6be02273272bf7ea2fd3ecdc30dcc1a6e7503f
nocommas555 <nocommas555@gmail.com>
2020-10-23 20:41:43 +0300

    add tmp filter to go through all clients

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def update_clients():
 	
 
 	clients = clients.build_query(filter=filter_params, sort = sort_params)
-	result = clients.execute()[:10]
+	result = clients.execute()
 
 	login_config = upwork.Config({\
 		'consumer_key': os.environ.get("ConsumerKey"),\
