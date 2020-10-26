7dd0d81ef4e680565837735f23ea3a3047f64d5b
nocommas555 <nocommas555@gmail.com>
2020-10-26 11:42:23 +0200

    fix get_todo_list_by_role

--- app.py
+++ app.py
@@ -1798,6 +1798,8 @@ def get_todo_list_by_role(token, roles):
 
 			# if person:
 			# d["stats"] = person[0]
+            if person["todo"]=="":
+                continue
 
 			d["todo_url"] = person["todo"].split()[1]
 			#handle links
