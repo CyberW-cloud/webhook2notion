59fe80bef48286cc4a771f75b90c7e257f0d223e
nocommas555 <nocommas555@gmail.com>
2020-10-31 21:52:13 +0200

    checking found clients

--- app.py
+++ app.py
@@ -2093,7 +2093,7 @@ def get_client_from_invite(invite):
 
 		buyer["skills"] = job_info["profile"]["op_required_skills"]["op_required_skill"]
 		buyer["ciphertext"] = ciphertext
-		buyer["questions"] = [x for x in job_info["profile"]["op_additional_questions"]]
+		buyer["questions"] = [x["op_additional_question"] for x in job_info["profile"]["op_additional_questions"]]
 		print(buyer["questions"])
 	except Exception as e:
 		print("Idk, some error while getting the client " + str(e))
