246ea15f844fa4dba41553001dc025b01c0c6ed1
nocommas555 <nocommas555@gmail.com>
2020-11-06 16:15:52 +0200

    testing pa autocomplete

--- app.py
+++ app.py
@@ -2138,6 +2138,7 @@ def create_invite(token, collection_url, subject, description, invite_to):
 
 	# row.to = invite_to
 	to_team_dir = team_directory.collection.get_rows(search=client.get_block(invite_to).name)
+	print(to_team_dir.get_browseable_url())
 	if len(to_team_dir)>0:
 		row.pa = team_directory.collection.get_rows(search=to_team_dir.pa[0].name).notion_user
 
