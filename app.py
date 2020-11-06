a219efba07b87f89cb43073226175b8cc9bbd3ea
nocommas555 <nocommas555@gmail.com>
2020-11-06 14:58:01 +0200

    testing getting all users of a space

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def email_report(subject, body):
 def tmp():
 	client = NotionClient(os.environ.get("TOKEN"))
 
-	for i in client.current_space.users:
+	for i in client.get_space("1ced0604-d477-49ab-b524-365de5e1069a").users:
 		print(i.full_name)
 
 	i = 1/0
