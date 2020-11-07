f52bff6fe9566687670ebb257d9f1dcb6139bdf7
nocommas555 <nocommas555@gmail.com>
2020-11-08 00:54:42 +0200

    add api endopint for js

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def get_room_messages():
 	if ac_user == None or ac_user not in token_clients.keys():
 		client = token_clients["safonov"]
 	else:
-		client = token_clients["ac_user"]
+		client = token_clients[ac_user]
 
 	messages_api = messageAPI(client) 	
 	user_api = userAPI(client)
